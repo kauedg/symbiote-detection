ea157c84fbb723efdee2b1a68efbca29fd54d5fd
Kaue Doretto Grecchi <31520323+kauedg@users.noreply.github.com>
2022-06-22 00:45:17 -0300

    Improvements
    now everything is bytes

--- rc4_decrypt/usb_h_decrypt.py
+++ rc4_decrypt/usb_h_decrypt.py
@@ -2,13 +2,10 @@
 
 import sys
 
-# Notice that the key is hardcoded. 
-# If you have the `usb.h` file chances are this is the correct encryption key, as it's an early version.
-
 def decryptRC4(data:str, key:str) -> str:
     S = list(range(256))
     j = 0
-    out = ""
+    out = b''
 
     #KSA Phase
     for i in range(256):
@@ -21,18 +18,17 @@ def decryptRC4(data:str, key:str) -> str:
         i = ( i + 1 ) % 256
         j = ( j + S[i] ) % 256
         S[i] , S[j] = S[j] , S[i]
-        out += (chr(ord(chr(char)) ^ S[(S[i] + S[j]) % 256]))
+        print(type(char))
+        out += (char ^ S[(S[i] + S[j]) % 256]).to_bytes(1, 'big')
 
     return out
        
 
 key = "caixasuporte4232" 
-
-# Each line is encrypted individually and delimited by '0x00000000'
 EOF = b'\x00\x00\x00\x00'
 
-encrypted_file = open(sys.argv[0], "rb")
-decrypted_file = open(sys.argv[1], 'w')
+encrypted_file = open(sys.argv[1], "rb")
+decrypted_file = open(sys.argv[2], 'wb')
 
 line = b''
 while True:
@@ -41,10 +37,8 @@ while True:
     if char:
         line += char
     else:
-        # end of file
         break
     
-    # check for delimiter
     if line[-4:] == EOF:
         line = line[:-4]
         decrypted_file.write(decryptRC4(line, key))
